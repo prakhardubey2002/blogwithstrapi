3ca4e2fa4633b866cef523302283d0550fd5b1ce
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-03-16 00:36:37 +0530

    fetched and displayed Single data

--- ninja-reviews/frontend/src/pages/ReviewDetails.js
+++ ninja-reviews/frontend/src/pages/ReviewDetails.js
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import {useParams} from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 export default function ReviewDetails() {
+  const {id } = useParams()
+  const {loading,error,data}=useFetch('http://localhost:1337/api/reviews/' + id)
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>error..:(</p>
+
+  console.log(data)
   return (
-    <h4>ReviewDetails</h4>
+    <div key={data.id} className="review-card">
+    <div className="rating">{data.attributes.rating}</div>
+    <h2>{data.attributes.title}</h2>
+    <small>console list</small>
+    <p>{data.attributes.body}...</p>
+
+  </div>
   )
 }
