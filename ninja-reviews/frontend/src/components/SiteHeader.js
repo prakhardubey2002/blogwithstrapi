25fcde105618eecb1395b486975d25f01845ed81
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-03-20 00:21:02 +0530

    UI Changed blog added in Strapi

--- ninja-reviews/frontend/src/components/SiteHeader.js
+++ ninja-reviews/frontend/src/components/SiteHeader.js
@@ -5,7 +5,7 @@ export default function SiteHeader() {
   return (
     <div className="site-header">
         <Link to="/" >
-            <h1>Ninja Reviews</h1>
+            <h1>Environment blogs</h1>
         </Link>
     </div>
   )
